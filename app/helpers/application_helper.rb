ba2cbe1f66976fcedf899065f58d467580f7df18
Tuomas Tuomitie <tuomas@tuomitie.com>
2017-02-22 23:53:28 +0200

    Vko6, teht. 8.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def edit_and_destroy_buttons(item)
+    unless current_user.nil?
+      edit = link_to('Edit', url_for([:edit, item]), class:"btn btn-primary omanappi")
+      del = link_to('Destroy', item, method: :delete,
+                    data: {confirm: 'Are you sure?' }, class:"btn btn-danger omanappi")
+      raw("#{edit} #{del}")
+    end
+  end
+
+  def round(param)
+    number_with_precision(param, precision: 1)
+  end
+
 end
